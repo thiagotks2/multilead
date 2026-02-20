88965c042cba4cfe94150f46243ca677a1b8b9d8
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2026-02-20 19:26:38 -0300

    instalação laravel e filament

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-# Criar pasta do socket e dar permissão
 mkdir -p /var/run/php
 chown www-data:www-data /var/run/php
 
-# O Node já está no PATH via apk, não precisa de NVM
-# O Laravel Installer já está no PATH via ENV no Dockerfile
-
-# Inicia o supervisor
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
--- public/index.php
+++ public/index.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-<body>
-    <h1>Teste</h1>
-</body>
+define('LARAVEL_START', microtime(true));
 
-</html>
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
