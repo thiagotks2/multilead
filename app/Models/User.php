0683864e117214c4185c444c8d6ca0eb588e3614
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2026-02-21 13:18:49 -0300

    fix(Model): Make the "active" field fillable in model

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,7 @@ class User extends Authenticatable implements FilamentUser
      * @var list<string>
      */
     protected $fillable = [
+        'active',
         'name',
         'email',
         'password',
