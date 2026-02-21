627b9dd5a6d77b0f2a7264f3f4fe8e73a53b4d72
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2026-02-21 12:49:44 -0300

    melhorando hot reaload no vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,25 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                'resources/views/**',
+                'app/Livewire/**',
+                'app/Filament/**',
+                'resources/lang/**',
+                'routes/**',
+            ],
         }),
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
         watch: {
+            usePolling: true,
+            interval: 100,
             ignored: ['**/storage/framework/views/**'],
         },
     },
