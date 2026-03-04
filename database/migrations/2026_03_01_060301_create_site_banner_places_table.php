0dec75a279e712019bf1ed978bf1b036655476fd
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2026-03-04 16:05:24 -0300

    refactor(banners): site_id field is optional

--- database/migrations/2026_03_01_060301_create_site_banner_places_table.php
+++ database/migrations/2026_03_01_060301_create_site_banner_places_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('site_banner_places', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('site_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('site_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->string('description')->nullable();
             $table->timestamps();
