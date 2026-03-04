b3e9b2152aa6c8375133d619824105750e9b594a
Thiago Cardoso <silva.thiagocardoso@gmail.com>
2026-03-04 15:55:58 -0300

    feat(baner): created banners migrations.

---
+++ database/migrations/2026_03_01_060301_create_site_banner_places_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('site_banner_places', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('site_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site_banner_places');
+    }
+};
